3fa6635c5e693b85efd589bfba915923a02e2218
YinMinghhh <yinming8086@gmail.com>
2026-04-05 23:14:50 +0800

    fix(trackeval): 修复JRDB评测工具的数据集导入

--- jrdb_toolkit/tracking_eval/TrackEval/trackeval/datasets/__init__.py
+++ jrdb_toolkit/tracking_eval/TrackEval/trackeval/datasets/__init__.py
@@ -1,13 +1,14 @@
-from .kitti_2d_box import Kitti2DBox
-from .kitti_mots import KittiMOTS
-from .mot_challenge_2d_box import MotChallenge2DBox
-from .mots_challenge import MOTSChallenge
-from .bdd100k import BDD100K
-from .davis import DAVIS
-from .tao import TAO
-from .youtube_vis import YouTubeVIS
-from .head_tracking_challenge import HeadTrackingChallenge
-from .rob_mots import RobMOTS
+# The JRDB release only vendors the JRDB dataset backends here.
+# Keep imports local to the available files so TrackEval can bootstrap.
+# from .kitti_2d_box import Kitti2DBox
+# from .kitti_mots import KittiMOTS
+# from .mot_challenge_2d_box import MotChallenge2DBox
+# from .mots_challenge import MOTSChallenge
+# from .bdd100k import BDD100K
+# from .davis import DAVIS
+# from .tao import TAO
+# from .youtube_vis import YouTubeVIS
+# from .head_tracking_challenge import HeadTrackingChallenge
+# from .rob_mots import RobMOTS
 from .jrdb_2d_box import JRDB2DBox
 from .jrdb_3d_box import JRDB3DBox
-
